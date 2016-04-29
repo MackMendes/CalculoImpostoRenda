1e4ef9ba666421f4f8ba166ad108c19a19ab8cba
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-04-28 23:26:29 -0300

    Add project files.

---
+++ test/CalculoImposto.Test/CalculaImpostoRendaTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculoImposto.Test
+{
+    [TestClass]
+    public class CalculaImpostoRendaTest
+    {
+        [TestMethod]
+        public void CalculaImportoFaixaSemTaxa()
+        {
+            // Organizar cenários
+            var impostoRenda = new ImpostoRenda();
+            var salario = 1499.15M; // Valor do Salário Limite
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = impostoRenda.Calcula(salario);
+
+            // Afirmar (Verificar resultado)
+            Assert.AreEqual(0M, parcelaADeduzir);
+        }
+    }
+}
