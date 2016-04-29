f6ec3c80f436a030dc6a38f6bb52da4740debdbc
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-04-28 23:47:01 -0300

    Passando 1ª parte...

---
+++ test/CalculoImposto.Test/ImpostoRenda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculoImposto.Test
+{
+    public class ImpostoRenda
+    {
+        public ImpostoRenda()
+        {
+        }
+
+        internal decimal Calcula(decimal salario)
+        {
+            var valorAReduzir = 0M;
+
+
+
+            return valorAReduzir;
+        }
+    }
+}
