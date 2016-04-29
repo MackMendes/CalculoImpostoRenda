12ea284366f93e9d60ae1884f10d38dabddc2c88
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-04-29 03:07:38 -0300

    Aplicação das regras, pensando como se todas as regras estivessem no banco de dados! (Pasta Domain)

---
+++ src/CalculoImposto.Domain/Entities/FaixaSalarialIRDomain.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculoImposto.Domain.Entities
+{
+    /// <summary>
+    /// Pensando em que os valores estarão em uma base de dados...
+    /// </summary>
+    public class FaixaSalarialIRDomain
+    {
+
+        public decimal PorcentoAliquota { get; set; }
+
+        public decimal ValorReduzirDoImposto { get; set; }
+
+        public decimal MenorSalarioDaFaixa { get; set; }
+
+        public decimal MaiorSalarioDaFaixa { get; set; }
+
+        public decimal CalcularIR(decimal salario)
+        {
+            if (salario >= MenorSalarioDaFaixa && salario <= MaiorSalarioDaFaixa)
+                return decimal.Round(((salario * this.PorcentoAliquota) - this.ValorReduzirDoImposto), 2);
+
+            return 0M;
+        }
+    }
+}
---
+++ test/CalculoImposto.Domain.Test/Entities/FaixaSalarialIRDomainTest.cs
@@ -0,0 +1,123 @@
+﻿using CalculoImposto.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculoImposto.Domain.Test.Entities
+{
+    /// <summary>
+    /// Executando um teste como se o problema fosse baseado em uma tabela de dados.
+    /// Levando em consideração que todas as regras de calculos de valores, estaram no banco de dados.
+    /// </summary>
+    [TestClass]
+    public class FaixaSalarialIRDomainTest
+    {
+        FaixaSalarialIRDomain _impostoRenda;
+
+        [TestInitialize]
+        public void Initializar()
+        {
+            _impostoRenda = new FaixaSalarialIRDomain();
+        }
+
+        [TestMethod]
+        public void FaixaSalarialIRDomain_CalculaImportoFaixaSemTaxa()
+        {
+            // Organizar cenários
+            var salario = 1499.15M; // Valor do Salário Limite
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = _impostoRenda.CalcularIR(salario);
+
+            // Afirmar (Verificar resultado)
+            var valorASerDeduzido = 0M;
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+        [TestMethod]
+        public void CalculaImportoFaixa7virgula5PorcentoAliquota()
+        {
+            // Base de cálculo mensal em R$: De 1.499,16 até 2.246,75
+            // Alíquota %: 7,5
+            // Parcela a deduzir do imposto em R$: 112,43
+
+            // Organizar cenários
+            var salario = 2246.75M; // Valor do Salário Limite
+
+            _impostoRenda.PorcentoAliquota = 0.075M;
+            _impostoRenda.ValorReduzirDoImposto = 112.43M;
+            _impostoRenda.MenorSalarioDaFaixa = 1499.16M;
+            _impostoRenda.MaiorSalarioDaFaixa = 2246.75M;
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = _impostoRenda.CalcularIR(salario);
+
+            // Afirmar (Verificar resultado)
+            var valorASerDeduzido = 56.08M; // (Salário * 0,075 - 112,43)
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+        [TestMethod]
+        public void CalculaImportoFaixa15PorcentoAliquota()
+        {
+            //Base de cálculo mensal em R$: De 2.246,76 até 2.995,70
+            //Alíquota %: 15,0
+            //Parcela a deduzir do imposto em R$: 280,94
+
+            // Organizar cenários
+            var salario = 2995.70M; // Valor do Salário Limite
+
+            _impostoRenda.PorcentoAliquota = 0.15M;
+            _impostoRenda.ValorReduzirDoImposto = 280.94M;
+            _impostoRenda.MenorSalarioDaFaixa = 2246.76M;
+            _impostoRenda.MaiorSalarioDaFaixa = 2995.70M;
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = _impostoRenda.CalcularIR(salario);
+
+            // Afirmar (Verificar resultado)
+            var valorASerDeduzido = 168.42M;
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+        [TestMethod]
+        public void CalculaImportoFaixa22Virgula5PorcentoAliquota()
+        {
+            // Base de cálculo mensal em R$: De 2.995,71 até 3.743,19
+            // Alíquota %: 22,5
+            // Parcela a deduzir do imposto em R$: 505,62
+
+            // Organizar cenários
+            var salario = 3743.19M; // Valor do Salário Limite
+
+            _impostoRenda.PorcentoAliquota = 0.225M;
+            _impostoRenda.ValorReduzirDoImposto = 505.62M;
+            _impostoRenda.MenorSalarioDaFaixa = 2995.71M;
+            _impostoRenda.MaiorSalarioDaFaixa = 3743.19M;
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = _impostoRenda.CalcularIR(salario);
+
+            // Afirmar (Verificar resultado)
+            var valorASerDeduzido = 336.60M;
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+        [TestMethod]
+        public void CalculaImportoFaixa27Virgula5PorcentoAliquota()
+        {
+            // Organizar cenários
+            var salario = 3743.20M; // Valor do Limite Inferior do Salário
+            
+            _impostoRenda.PorcentoAliquota = 0.275M;
+            _impostoRenda.ValorReduzirDoImposto = 692.78M;
+            _impostoRenda.MenorSalarioDaFaixa = 3743.19M;
+            _impostoRenda.MaiorSalarioDaFaixa = decimal.MaxValue;
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = _impostoRenda.CalcularIR(salario);
+
+            // Afirmar (Verificar resultado)
+            var valorASerDeduzido = 336.60M;
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+    }
+}
