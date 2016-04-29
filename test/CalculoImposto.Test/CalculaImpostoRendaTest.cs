31da59fe2c7c41d5d9bd236ff945a16f37f204a1
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-04-29 00:17:28 -0300

    Segunda teste passando... e indo para o terceiro teste. E aplicando um pouco de refatorações...

--- test/CalculoImposto.Test/CalculaImpostoRendaTest.cs
+++ test/CalculoImposto.Test/CalculaImpostoRendaTest.cs
@@ -16,7 +16,48 @@ namespace CalculoImposto.Test
             decimal parcelaADeduzir = impostoRenda.Calcula(salario);
 
             // Afirmar (Verificar resultado)
-            Assert.AreEqual(0M, parcelaADeduzir);
+            var valorASerDeduzido = 0M;
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
         }
+
+        [TestMethod]
+        public void CalculaImportoFaixa7virgula5PorcentoAliquota()
+        {
+            // Organizar cenários
+            var impostoRenda = new ImpostoRenda();
+            var salario = 2246.75M; // Valor do Salário Limite
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = impostoRenda.Calcula(salario);
+
+            // Afirmar (Verificar resultado)
+            var porcentoAliquota = 0.075M; // 7,5%
+            var valorReduzirDoImposto = 112.43M;
+            var valorASerDeduzido = decimal.Round((salario * porcentoAliquota - valorReduzirDoImposto), 2);
+
+            //var valorASerDeduzido = 56.08M; // (Salário * 0,075 - 112,43)
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+
+        [TestMethod]
+        public void CalculaImportoFaixa15PorcentoAliquota()
+        {
+            // Organizar cenários
+            var impostoRenda = new ImpostoRenda();
+            var salario = 2995.70M; // Valor do Salário Limite
+
+            // Ação (Excutar)
+            decimal parcelaADeduzir = impostoRenda.Calcula(salario);
+
+            // Afirmar (Verificar resultado)
+            var porcentoAliquota = 0.15M; // 15%
+            var valorReduzirDoImposto = 280.94M;
+            var valorASerDeduzido = decimal.Round((salario * porcentoAliquota - valorReduzirDoImposto), 2);
+
+            Assert.AreEqual(valorASerDeduzido, parcelaADeduzir);
+        }
+
+
     }
 }
--- test/CalculoImposto.Test/ImpostoRenda.cs
+++ test/CalculoImposto.Test/ImpostoRenda.cs
@@ -8,13 +8,18 @@ namespace CalculoImposto.Test
         {
         }
 
-        internal decimal Calcula(decimal salario)
+        public decimal Calcula(decimal salario)
         {
             var valorAReduzir = 0M;
-
-
+            if (salario >= 1499.16M && salario <= 2246.75M)
+                return this.CalculaImposto(salario, 0.075M, 112.43M);
 
             return valorAReduzir;
+        }
+
+        private decimal CalculaImposto(decimal salario, decimal percentualAliquota, decimal parcelaReduzirImposto)
+        {
+            return decimal.Round(((salario * percentualAliquota) - parcelaReduzirImposto), 2);
         }
     }
 }
